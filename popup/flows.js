c9e08146ef6f8fda92f619699a59426c0ec0d1f8
JulianKer <jschmuker742@alumno.unlam.edu.ar>
2025-07-20 21:05:14 -0300

    10 - autolaunched listo + refactor de otros.

--- popup/flows.js
+++ popup/flows.js
@@ -75,14 +75,14 @@ function setupCopyButton(buttonId, inputId) {
     }
 
     const optimizacionMap = {
-      "actions_and_related_records": "COU",
+      "actions_and_related_records": "ARR",
       "fast_field_updates": "FFU"
     };
 
     const triggerMap = {
       "created": "CRE",
       "updated": "UPD",
-      "created_or_updated": "ARR",
+      "created_or_updated": "COU",
       "deleted": "DEL"
     };
 
@@ -146,5 +146,50 @@ document.getElementById("objeto_scf").addEventListener("input", generarLabelYApi
 document.getElementById("accion_scf").addEventListener("input", generarLabelYApiNameSCF);
 setupCopyButton("copyBtnOutputLabelSCF", "label_scf");
 setupCopyButton("copyBtnOutputApiSCF", "apiName_scf");
+
+//--------------------------------------------------------------------
+
+
+//---------------------------- Autolaunched ------------------------
+
+function generarLabelYApiNameALF() {
+  const labelElement = document.getElementById("label_alf");
+  const apiElement = document.getElementById("apiName_alf");
+
+  const accion = document.getElementById("accion_alf").value.trim();
+  const descripcion = document.getElementById("descripcion_alf").value.trim();
+  const checkboxes = document.querySelectorAll('#invocadores_alf input[type="checkbox"]:checked');
+
+  const invocadores = Array.from(checkboxes).map(cb => cb.value.toUpperCase());
+
+  if (!accion || !descripcion || invocadores.length === 0) {
+    labelElement.value = "Debe completar todos los campos.";
+    apiElement.value = "Debe completar todos los campos.";
+    return;
+  }
+
+  const sufijos = `[${invocadores.join(", ")}]`;
+  const label = `${accion} [ALF] - ${descripcion} | ${sufijos}`;
+  const apiName = formatToApiName(label);
+
+  if (label.length > 80) {
+    labelElement.value = "Label too long... Max length 80.";
+    apiElement.value = "Label too long... Max length 80.";
+  } else {
+    labelElement.value = label;
+    apiElement.value = apiName;
+  }
+}
+
+// Eventos
+document.getElementById("accion_alf").addEventListener("input", generarLabelYApiNameALF);
+document.getElementById("descripcion_alf").addEventListener("input", generarLabelYApiNameALF);
+document.querySelectorAll('#invocadores_alf input[type="checkbox"]').forEach(cb =>
+  cb.addEventListener("change", generarLabelYApiNameALF)
+);
+
+// Botones de copiar
+setupCopyButton("copyBtnOutputLabelALF", "label_alf");
+setupCopyButton("copyBtnOutputApiALF", "apiName_alf");
 
 //--------------------------------------------------------------------
